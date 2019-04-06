229b9e5319e308929133e482afdd64a6cc98a583
gaborszekely <gabor_szekely@hotmail.com>
2019-04-06 13:47:24 -0600

    Finished Photos tab section

--- client/components/tabs/qanda/index.jsx
+++ client/components/tabs/qanda/index.jsx
@@ -1,23 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+
 import TabHeader from '../../TabHeader';
+import BodyWrapper from '../BodyWrapper';
+import QuestionsBody from './QuestionsBody/index';
 
 import { upper__wrapper } from '../../css/main.scss';
 
-class QAndA extends Component {
-  state = {};
-
-  render() {
-    return (
-      <>
-        <div className={upper__wrapper}>
-          <TabHeader title="Q &amp; A" buttonText="Ask a Question" />
-          <div>
-            <p>Content</p>
-          </div>
-        </div>
-      </>
-    );
-  }
-}
+const QAndA = () => (
+  <>
+    <div className={upper__wrapper}>
+      <TabHeader title="Q &amp; A" buttonText="Ask a Question" />
+    </div>
+    <div>
+      <BodyWrapper>
+        <QuestionsBody />
+      </BodyWrapper>
+    </div>
+  </>
+);
 
 export default QAndA;
